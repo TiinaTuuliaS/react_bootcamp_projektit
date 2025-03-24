72ecea6e261a568bbb86da280ca0266077655f17
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-03-24 18:05:22 +0200

    nodemon saatu toimimaan, kaikki nodeserverit tapettu

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Server is ready");
+app.get("/products", (req,res) => {
+    res.send("Server is ready 123");
 })
 
 app.listen(5000, () => {
