1e6519f0133dfaeb205fa7e7ba24aeb5999e62c1
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-03-25 15:07:32 +0200

    crud toimintojen tekeminen put ja delete, testaus postmanilla samalla

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(express.json()); //Json data vastaanotetaan req.body elementissä
 
-app.get("api/products", async (req, res) => {
+// Middleware, jotta saadaan JSON-data käyttöön
+app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
 try {
     const products = await Product.find({});
     res.status(200).json({ success: true, data: products });
@@ -20,9 +23,6 @@ try {
 }
 
 })
-
-// Middleware, jotta saadaan JSON-data käyttöön
-app.use(express.json());
 
 app.post("/api/products", async (req, res) => {
     const product = req.body; // Käyttäjän lähettämä data kun tuote luodaan
@@ -41,6 +41,24 @@ app.post("/api/products", async (req, res) => {
         res.status(500).json({ success: false, message: "Virhe tietojen tallentamisessa" });
     }
 });
+
+app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) { 
+        return res.status(404).json({ success: false, message: "Id numeroa ei ole olemassa" });
+    }
+
+    try {
+        const uptatedProduct  = await Product.findByIdAndUpdate(id, product, {new: true});
+        res.status(200).json({ success: true, data: uptatedProduct, message: "Tuote päivitetty onnistuneesti" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Virhe tuotteet päivityksessä"});
+    }
+});
+
 
 app.delete("/api/products/:id", async (req, res) => {
     const { id } = req.params;
